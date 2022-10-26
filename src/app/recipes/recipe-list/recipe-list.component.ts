31bea75e33ba62bd15d0cf39997bab4c2828a649
Siva Narisetty <sivanarisetty@geotab.com>
2022-10-26 09:37:22 -0400

    The basics - recipes and shopping list added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../models/recipe';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('Tandoori Chicken', 'A healthy Indian chicken recipe', 'https://hips.hearstapps.com/hmg-prod/images/chicken-tandori-1526595014.jpg'),
+    new Recipe('Carror Salad', 'A healthy carrot salad with lime', 'https://www.eatwell101.com/wp-content/uploads/2020/11/Shredded-Carrot-Salad-recipe.jpg')
+  ];
+    
   constructor() { }
 
   ngOnInit(): void {
